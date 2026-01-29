e6c4ba4a97c6b3ea4bb25427011f1dbc93473598
ANGELOREYES-afk <angelohenaoreyes@yahoo.com>
2026-01-29 14:47:08 -0600

    left and right pedal as5600 configured

--- as5600/as5600.ino
+++ as5600/as5600.ino
@@ -1,17 +1,43 @@
 #include <Wire.h>
 #include <AS5600.h>
 
-AS5600 as5600;
+// Analog Sensor 1
+const int analogPin = A0;
+
+// I2C sensor 2
+AS5600 as5600;   
 
 void setup() {
   Serial.begin(115200);
+  
+  // Initialize I2C
   Wire.begin();
-  as5600.begin();   // I2C addr is fixed at 0x36 for AS5600 [page:1]
+
+  // Initialize the I2C Sensor
+  // as5600 lib func .begin() is to check if found
+  if (!as5600.begin()) {
+    Serial.println("Error: I2C Sensor (Sensor 2) not found!");
+  }
+  // not necessary but great for controlling what you get 
+  as5600.setDirection(AS5600_CLOCK_WISE); 
 }
 
 void loop() {
-  double angle = as5600.readAngle();
-  double conversion = 360.0/4096.0;
-  Serial.println(angle * conversion);
-  delay(200);
+
+  int analogRaw = analogRead(analogPin);
+  // Convert 0-1023 to 0-360
+  float angle1 = analogRaw * (360.0 / 1023.0);
+
+  // Convert 4096 to 0-350
+  float angle2 = as5600.readAngle() * (360.0 / 4096.0);
+
+  Serial.print("Sensor 1 (Analog): ");
+  Serial.print(angle1, 1);
+  
+  Serial.print("\t | \t"); // Spacer
+  
+  Serial.print("Sensor 2 (I2C): ");
+  Serial.println(angle2, 2); // 2 decimal places
+
+  delay(100);
 }
